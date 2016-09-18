0a9eaa56251e1558e1ac311902744474f56c1249
raja1995 <rajaece154@gmail.com>
2016-09-18 23:56:25 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,1 +1,14 @@
 
+public class array{
+  public static void main(String args[])
+  {
+    Scanner in=new Scanner(System.in)
+    int num=in.nextInt();
+    int fact=1;
+    for(int i=1;i<=num;i++)
+    {
+      fact=fact*i;
+    }
+    System.out.print(fact);
+  }
+}
